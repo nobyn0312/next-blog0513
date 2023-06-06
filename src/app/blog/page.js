2fe0ea2f9db6f8999c86db4a85ec7407b8b1114e
nobyn0312 <nobyn0312@gmail.com>
2023-06-06 09:37:31 +0900

    nextblog

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,14 +1,34 @@
 import Hero from "components/Hero";
 import Container from "components/container";
 import Meta from "components/meta";
+import {client} from "/lib/client"
 
-export default function blog(){
+// SSG
+export const getStaticProps =async()=>{
+  const data =await client.get({endpoint:"nextblog0513"});
+  return{
+    props:{
+      blog:data,
+    },
+  }
+}
+
+export default function blog({blog}){
   return(
     <Container>
           <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
       <Hero title="Blog"
         subtitle="ブログ アウトプットしていく"
       />
+      <div>
+        {blog.map((blog)=>(
+        <li key={blog.id}>
+        <a href="">{blog.title}</a>
+        </li>
+        ))  
+      </div>
+
     </Container>
+  
   )
 }
